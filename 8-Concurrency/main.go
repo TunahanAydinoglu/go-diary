f72d41ad281537fc3f46035e38b8cea38002680c
Tunahan <tunahan.aydinoglu@yemeksepeti.com>
2021-07-12 01:07:17 +0300

    concurrency

--- 8-Concurrency/main.go
+++ 8-Concurrency/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	callGoRoutineFunc()
+	fmt.Println("---------------------------------------")
+	callParallelRoutineFunc()
+}
+
+func goroutineExFunc() {
+	for index := byte('a'); index <= byte('f'); index++ {
+		fmt.Println(string(index))
+	}
+}
+
+func callGoRoutineFunc() {
+	go goroutineExFunc()
+	fmt.Println("Hi") //is printed first
+	go fmt.Println("Say hello")
+	time.Sleep(100 * time.Millisecond)
+}
+
+func parallelExFunc() {
+	for index := byte('a'); index <= byte('f'); index++ {
+		go fmt.Println(string(index))
+	}
+}
+func callParallelRoutineFunc() {
+	go parallelExFunc()
+	fmt.Println("Hi")
+	go fmt.Println("Say hello")
+	time.Sleep(100 * time.Millisecond)
 }
