8c9cc8cc76b6aa646e9baa453e02e1a2510e5f31
Tunahan <tunahan.aydinoglu@yemeksepeti.com>
2021-07-14 00:08:30 +0300

    net http

--- 2-net-http/1-Hello Net/main.go
+++ 2-net-http/1-Hello Net/main.go
@@ -4,9 +4,20 @@ import (
 	"net/http"
 )
 
+func profileHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Profile Page"))
+	w.WriteHeader(http.StatusOK)
+
+	path := r.URL.Path
+	w.Write([]byte(path))
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello Net Http!"))
 	})
+
+	http.HandleFunc("/profile", profileHandler)
+
 	http.ListenAndServe(":8080", nil)
 }
